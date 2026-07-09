79a3bfcfdbc4b97b211cf6371034e3ee491e8fa4
ayush <ayushtomar8624@gmail.com>
2026-07-10 00:34:31 +0530

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,11 @@
 import { Providers } from "@/app/providers";
 import { AppRoutes } from "@/app/routes";
-import { Analytics } from '@vercel/analytics/next';
 
 function App() {
   return (
-    <>
-      <Analytics />
       <Providers>
         <AppRoutes />
       </Providers>
-    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '@/app/App.tsx'
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
